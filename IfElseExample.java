41dea9b32b039f3bef031a95797964e8add031a8
Maneesha-Esaram <maneeshaesaram7@gmail.com>
2026-01-31 13:56:42 +0530

    Create IfElseExample.java

---
+++ IfElseExample.java
@@ -0,0 +1,11 @@
+class IfElseExample{
+  public static void main(String[] args){
+    int age=18;
+    if(age>=18){
+      System.out.println("Eligible for vote");
+    }
+    else{
+      System.out.pritnln("not eligible for vote")
+        }
+  }
+}
